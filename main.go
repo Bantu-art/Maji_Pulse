366502dbd5d56d7685d18c8035c7427fbe233fde
hokwach <hezronokwach@gmail.com>
2024-07-30 14:07:58 +0300

    Add main function to handle the route

--- main.go
+++ main.go
@@ -1,9 +1,24 @@
 package main
 
+import (
+	"log"
+	"net/http"
+	"smart-water-management/internal/web"
+)
+
 func main() {
-	// var set struct {
-	// 	protocol string
-	// 	sets     string
-	// }
-	// set := myfunc
+	web.LoadTemplates()
+
+	http.HandleFunc("/", web.HomePage)
+	//http.HandleFunc("/user", web.UserPage)
+	http.HandleFunc("/admin", web.AdminPage)
+	http.HandleFunc("/api/flowrates", web.GetAreaFlowRates)
+	http.HandleFunc("/api/fairdistribution", web.GetFairDistribution)
+	http.HandleFunc("/api/leakdetection", web.GetLeakDetection)
+
+	// Serving static files for JavaScript and CSS
+	http.Handle("/static/", http.StripPrefix("/static/", http.FileServer(http.Dir("static"))))
+
+	log.Println("Server started on :8080")
+	log.Fatal(http.ListenAndServe(":8080", nil))
 }
